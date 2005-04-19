3b192068e0be39cf27c6999ea25b11e5238a817c
Martin Bays <mbays@freeshell.org>
2005-04-19 20:35:40 +0000

    export only defined functions

--- lib/lib.h
+++ lib/lib.h
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#ifndef HAVE_STRLCAT
 size_t strlcat(char *, const char *, size_t);
+#endif
+#ifndef HAVE_STRLCPY
 size_t strlcpy(char *, const char *, size_t);
-#ifdef LINUX
+#endif
+#ifndef HAVE_FGETLN
 char *fgetln(FILE *, size_t *);
 #endif
